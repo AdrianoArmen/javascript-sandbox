adfb0bccc144284e1584e2d19cc710597a329954
AdrianoA <adriano.armendaris@gmail.com>
2021-09-13 23:02:34 -0500

    quotes on strings

--- Curso JS Moderno/03-Strings/js/01-app.js
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -1,6 +1,6 @@
 // if its inside quate its a string
-const product = "Monitor 20 pulgadas";
-const product2 = String('Monitor 24 Pulgadas');
+const product = "Monitor 20 pulgadas\"";
+const product2 = String('Monitor 24 Pulgadas"');
 
 const product3 = new String('Monitor 27 Pulgadas');
 
