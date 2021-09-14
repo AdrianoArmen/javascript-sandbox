cabd72d144f376bd21de7d176bdc755e2644cc3c
AdrianoA <adriano.armendaris@gmail.com>
2021-09-13 23:01:25 -0500

    strong declaration ways

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,9 @@
+// if its inside quate its a string
+const product = "Monitor 20 pulgadas";
+const product2 = String('Monitor 24 Pulgadas');
+
+const product3 = new String('Monitor 27 Pulgadas');
+
+console.log (product);
+console.log (product2);
+console.log (product3);
