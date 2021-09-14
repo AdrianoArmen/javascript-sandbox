dfac73c637e27b445a28f3b2e5f4d491d74cdd1f
AdrianoA <adriano.armendaris@gmail.com>
2021-09-13 22:26:59 -0500

    console excercise

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,2 +1,6 @@
 const nombre = prompt('Cual es tu nombre?');
 document.querySelector('.contenido').innerHTML=`${nombre} está aprendiendo JavaScript moderno`;
+
+// console.log > prints
+// console.time > time takes a process
+// console. warn > warning in console
