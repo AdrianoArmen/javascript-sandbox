de25f7c545b931c52707121c74441da7d407571d
AdrianoA <adriano.armendaris@gmail.com>
2021-09-14 23:44:25 -0500

    indexof and includes methods

--- Curso JS Moderno/03-Strings/js/02-app.js
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -1,3 +1,10 @@
 const product = 'Monitor 20 Pulgadas';
 
+// number of letter in string
 console.log(product.length);
+
+// in what index is located in stringh
+console.log(product.indexOf('Monitor'));
+
+// if its included on strings
+console.log(product.includes('Monitor'));
