3a55fb35f678818066367da1cbea73656a0bcb11
AdrianoA <adriano.armendaris@gmail.com>
2021-09-14 23:49:21 -0500

    complex concats

--- Curso JS Moderno/03-Strings/js/03-app.js
+++ Curso JS Moderno/03-Strings/js/03-app.js
@@ -4,3 +4,9 @@ const price = '30 USD';
 
 // put toguether 2 strings
 console.log(product.concat(price));
+console.log(product.concat('On Sale'));
+
+// also concat
+console.log(product+price+"On Sale!!");
+
+console.log("The product "+ product +"has a price of "+price);
