b8ef4cee2e89b306829b7286af13434d64d1290b
AdrianoA <adriano.armendaris@gmail.com>
2021-09-15 00:03:09 -0500

    replace string method

---
+++ Curso JS Moderno/03-Strings/js/05-app.js
@@ -0,0 +1,7 @@
+const product = 'Lightsaber 20 Inches';
+
+console.log(product);
+
+// replace text on string
+console.log(product.replace('Inches','"'));
+console.log(product.replace('Lightsaber','Vibroblade'));
