ccd494fc8f29733cfbca11990e3c92f32392794b
AdrianoA <adriano.armendaris@gmail.com>
2021-09-15 00:05:51 -0500

    slice and substring method

--- Curso JS Moderno/03-Strings/js/05-app.js
+++ Curso JS Moderno/03-Strings/js/05-app.js
@@ -5,3 +5,11 @@ console.log(product);
 // replace text on string
 console.log(product.replace('Inches','"'));
 console.log(product.replace('Lightsaber','Vibroblade'));
+
+// slice to cut
+console.log(product.slice(0,5));
+// firs tindex is where it starts and last index is where ends the cut
+
+
+// slice alternative
+console.log(product.substring(0,5));
