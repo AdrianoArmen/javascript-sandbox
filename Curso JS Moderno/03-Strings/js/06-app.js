026cfffb92c2090b4a1136e9ed90b6e100362a71
AdrianoA <adriano.armendaris@gmail.com>
2021-09-17 20:23:51 -0500

    .split string method

--- Curso JS Moderno/03-Strings/js/06-app.js
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -2,8 +2,20 @@ const product = 'Lightsaber 20 Inches';
 
 // repeat string chain
 const text = ' on Sale!'.repeat(3);
+// if not interger number js will round up to next interger
+
 
 // 3 times on Sale
 console.log(text);
 
 console.log(`${product} ${text} !!!`);
+
+// split makes you divide string
+
+const activity = "Im learning Modern JavaScript";
+
+// divides by space " "
+console.log(activity.split(" "));
+
+const hobbies = 'Read, walk, sing, write, code';
+console.log(hobbies.split(", "));
