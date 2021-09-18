f2a752843ec50368302a641c2421950931c444ec
AdrianoA <adriano.armendaris@gmail.com>
2021-09-17 20:20:17 -0500

    .repeat string method

---
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -0,0 +1,9 @@
+const product = 'Lightsaber 20 Inches';
+
+// repeat string chain
+const text = ' on Sale!'.repeat(3);
+
+// 3 times on Sale
+console.log(text);
+
+console.log(`${product} ${text} !!!`);
