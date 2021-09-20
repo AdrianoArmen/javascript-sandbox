d9f40043b38f6d90856d53de68c49fc4456af11d
AdrianoA <adriano.armendaris@gmail.com>
2021-09-19 23:30:59 -0500

    number creation

---
+++ Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+// number creation
+
+const number1 = 20;
+const number2 = "20";
+const number3 = 20.20;
+const number4 = .534520;
+
+// number
+console.log(number1);
+// string - not number
+console.log(number2);
+// floating number
+console.log(number3);
+// also number
+console.log(number2);
