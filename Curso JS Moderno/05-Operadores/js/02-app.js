eca55222092e2e469ab82d88853f656b25373b60
AdrianoA <adriano.armendaris@gmail.com>
2021-09-27 00:40:29 -0500

    doble equal coparer operator

--- Curso JS Moderno/05-Operadores/js/02-app.js
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -1,3 +1,7 @@
 const number1 = 20;
 const number2 = "20";
 const number3 = 30;
+
+// check if 2 numbers are equal / one equal only assig value to av ariable so
+// false
+console.log(number1==number3);
