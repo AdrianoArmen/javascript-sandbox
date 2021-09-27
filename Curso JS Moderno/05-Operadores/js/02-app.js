e58c2ad7c6546ba4702225d8ca21743e69268c6a
AdrianoA <adriano.armendaris@gmail.com>
2021-09-27 01:00:36 -0500

    strong and number true comparison

--- Curso JS Moderno/05-Operadores/js/02-app.js
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -5,3 +5,8 @@ const number3 = 30;
 // check if 2 numbers are equal / one equal only assig value to av ariable so
 // false
 console.log(number1==number3);
+
+// will give true since its not a strict comparison
+console.log(number1==number2);
+
+
