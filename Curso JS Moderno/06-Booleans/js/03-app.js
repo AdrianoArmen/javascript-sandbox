205d940c0b1a3b4eee317c5d20ca53d5919b72be
AdrianoA <adriano.armendaris@gmail.com>
2021-10-03 03:27:39 -0500

    bad practice booleand true if statement

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,8 @@
+const authenticated = true;
+
+// bad practice authenticated true is understood
+if (authenticated===true) {
+    console.log("you can watch Netflix")
+} else {
+    console.log("you can't watch")
+}
