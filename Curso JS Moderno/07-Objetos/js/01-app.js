1a9968879e25e51b684d1dd2a5758f42ea263ff5
AdrianoA <adriano.armendaris@gmail.com>
2021-10-04 00:07:36 -0500

    object literal

---
+++ Curso JS Moderno/07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const name = "30 inch LIghtsaber";
+const price = 300;
+const available = true ;
+
+// object groups everything in one variable
+
+// object literal
+const product = {
+name: "30 inch lightsaber",
+price: 300,
+available: true
+}
+
+console.log(product);
