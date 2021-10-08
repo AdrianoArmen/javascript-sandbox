cd08f95a6eaed4223ba146e8ec67c56fb6862054
AdrianoA <adriano.armendaris@gmail.com>
2021-10-08 01:14:29 -0500

    object insede object

--- Curso JS Moderno/07-Objetos/js/05-app.js
+++ Curso JS Moderno/07-Objetos/js/05-app.js
@@ -1,6 +1,11 @@
 const product = {
-    name: "30 inch lightsaber",
-    price: 300,
-    available: true
-    }
-    
+  name: "30 inch lightsaber",
+  price: 300,
+  available: true,
+  information: {
+    peso: "1 kg",
+    measure: "1 meter",
+  },
+};
+
+console.log(product);
