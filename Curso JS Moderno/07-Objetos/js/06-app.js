40ce4c09269f2deb12e68006d7857f5b203483a8
AdrianoA <adriano.armendaris@gmail.com>
2021-10-09 01:05:46 -0500

    excercise 6 setup

---
+++ Curso JS Moderno/07-Objetos/js/06-app.js
@@ -0,0 +1,14 @@
+const product = {
+    name: "30 inch lightsaber",
+    price: 300,
+    available: true,
+    information: {
+      peso: "1 kg",
+      measure: "1 meter",
+      factory: {
+        country: "China",
+      },
+    },
+  };
+  
+  console.log(product);
