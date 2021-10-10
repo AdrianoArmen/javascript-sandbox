3e88aa5780cf59617e8439ac684823f6ebd5ef07
AdrianoA <adriano.armendaris@gmail.com>
2021-10-09 23:43:52 -0500

    strict mode

--- Curso JS Moderno/07-Objetos/js/08-app.js
+++ Curso JS Moderno/07-Objetos/js/08-app.js
@@ -1,5 +1,17 @@
+"use strict";
+// strict mode will ensure better practices are enforced
+
+
 const product = {
-    name: "30 inch lightsaber",
-    price: 300,
-    available: true
-    }
+  name: "30 inch lightsaber",
+  price: 300,
+  available: true,
+};
+
+// changing objects properties
+product.available = false;
+product.image = "image.jpg";
+
+console.log(product);
+
+
