404a8e430dd8e8b2b511286a92835b0d49dc3577
AdrianoA <adriano.armendaris@gmail.com>
2021-10-10 00:34:07 -0500

    object method assign

--- Curso JS Moderno/07-Objetos/js/10-app.js
+++ Curso JS Moderno/07-Objetos/js/10-app.js
@@ -1,5 +1,18 @@
 const product = {
-    name: "30 inch lightsaber",
-    price: 300,
-    available: true,
-  };
+  name: "30 inch lightsaber",
+  price: 300,
+  available: true,
+};
+
+const measures = {
+  distance: "1m",
+  weight: "20kg",
+};
+
+console.log(product);
+console.log(measures);
+
+// object method assing will combine objects in one variable
+const group = Object.assign(product,measures);
+
+console.log(group);
