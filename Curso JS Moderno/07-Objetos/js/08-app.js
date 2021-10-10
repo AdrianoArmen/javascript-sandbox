16becedb77af82f4a794a0c9d329f1730ccef72e
AdrianoA <adriano.armendaris@gmail.com>
2021-10-09 23:45:53 -0500

    object method freeze

--- Curso JS Moderno/07-Objetos/js/08-app.js
+++ Curso JS Moderno/07-Objetos/js/08-app.js
@@ -8,6 +8,10 @@ const product = {
   available: true,
 };
 
+// prevent object from being modified - object method freeze
+Object.freeze(product);
+
+
 // changing objects properties
 product.available = false;
 product.image = "image.jpg";
