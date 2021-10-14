ecdad3b03f0e3afb5a020926ae6dafdccca287dd
AdrianoA <adriano.armendaris@gmail.com>
2021-10-13 21:32:29 -0500

    multimple objects scope example

--- Curso JS Moderno/07-Objetos/js/11-app.js
+++ Curso JS Moderno/07-Objetos/js/11-app.js
@@ -7,6 +7,20 @@ console.log(`The product: ${this.name} has a price of: ${this.price}`)
     }
   };
 
-  product.showInfo();
+
 // template literal 
 // THIS scopes to object - to refers the object itself
+
+const product2 = {
+  name: "Vibroblade",
+  price: 3000,
+  available: false,
+  showInfo: function() {
+console.log(`The product: ${this.name} has a price of: ${this.price}`)
+  }
+};
+
+// very useful to use same function on different objects and refer to inner scope
+
+product.showInfo();
+product2.showInfo();
