9a13766a4830e6d19bc56106ae51d2dcfbea0284
AdrianoA <adriano.armendaris@gmail.com>
2021-10-15 23:28:44 -0500

    object cosntructor vs object literal

--- Curso JS Moderno/07-Objetos/js/12-app.js
+++ Curso JS Moderno/07-Objetos/js/12-app.js
@@ -1,11 +1,15 @@
-const product = {
-    name: "30 inch lightsaber",
-    price: 300,
-    available: true,
-  };
+// object literal
 
-  const product = {
-    name: "30 inch lightsaber",
-    price: 300,
-    available: true,
-  };
+const product = {
+  name: "30 inch lightsaber",
+  price: 300,
+  available: true,
+};
+
+// object constructor
+
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+  this.available = true;
+}
