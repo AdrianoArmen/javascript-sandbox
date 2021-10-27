b8a3f0a2a33b9b731852979ed564d416bce3ab66
AdrianoA <adriano.armendaris@gmail.com>
2021-10-27 00:34:23 -0500

    Object.entries method

--- Curso JS Moderno/07-Objetos/js/13-app.js
+++ Curso JS Moderno/07-Objetos/js/13-app.js
@@ -11,3 +11,6 @@ const product = {
 
 // will return array with values
   console.log(Object.values( product))
+
+  // will return array with both values and keys in pairs
+  console.log(Object.entries( product))
