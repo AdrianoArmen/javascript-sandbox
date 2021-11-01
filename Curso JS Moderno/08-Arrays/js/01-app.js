4e9c6584cc401fae31b271479152df05d2bd6f28
AdrianoA <adriano.armendaris@gmail.com>
2021-10-31 19:38:09 -0600

    objects vs arrays

--- Curso JS Moderno/08-Arrays/js/01-app.js
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -1,2 +1,9 @@
 // arrays group element of same type - multiple similar elements - brackets
  const numbers = [3, 6, 8];
+
+// vs objects that come in curly brackets
+const product = {
+    name: "30 inch lightsaber",
+    price: 300,
+    available: true,
+  };
